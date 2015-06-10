358ef4815531176504dec1b4eec0b13870105164
Flenix <flenix@silvania.co.uk>
2015-06-10 15:17:15 +0100

    Texture rendering issues

--- src/main/java/co/uk/silvania/mcheliparts/MCHPTextItem.java
+++ src/main/java/co/uk/silvania/mcheliparts/MCHPTextItem.java
@@ -4,19 +4,20 @@ import java.util.List;
 
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
-public class MCHPTextItem extends MCHPItems {
+public class MCHPTextItem extends Item {
 	
 	public String cal = "";
 	public int stack;
 	public String texture;
 
 	public MCHPTextItem(String cal, int stack, String texture) {
-		super(64);
+		super();
 		this.cal = cal;
 		if (stack > 1) {
 			this.stack = stack;
